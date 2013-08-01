4dbc3dd18bdb9321712031d7fdc74fcec642d349
Russell John-Baptiste <luckvery@gmail.com>
2013-08-01 03:54:49 +0000

    Displaying hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var buffer = new buffer();
+  response.send(buffer.toString('utf-8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
